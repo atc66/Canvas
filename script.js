01ff426de809cf3ce11c0a598ea8f85690b2eb08
aciukurescu <andrewciukurescu@gmail.com>
2018-04-18 20:46:35 -0400

    first commit, drew trianlge inside rect

---
+++ script.js
@@ -0,0 +1,12 @@
+var c = document.querySelector('#c');
+var ctx = c.getContext("2d")
+
+ctx.strokeRect(50,50,100,100);
+
+ctx.beginPath();
+ctx.moveTo(75,75);
+ctx.lineTo(125,75);
+ctx.lineTo(125,125);
+ctx.lineTo(75,75);
+ctx.fill();
+
